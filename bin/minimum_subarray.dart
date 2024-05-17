260f8c91a06a01dc97813c8608027ee1cc7580e1
erivanstdio <erivanstdio@gmail.com>
2024-05-17 19:45:19 -0300

    adding comment and changing input for better understanding of the algorith usage

--- bin/minimum_subarray.dart
+++ bin/minimum_subarray.dart
@@ -2,7 +2,8 @@ import 'package:minimum_subarray/minimum_subarray.class.dart';
 
 void main() {
 
-  MinimumSubarray targetAndArray = MinimumSubarray(target: 7, numbersArray: [2,3,1,2,4,3]);
+  // expect the minimum subarray length to be 9:
+  MinimumSubarray targetAndArray = MinimumSubarray(target: 50, numbersArray: [2,4,7,8,9,5,3,4,6,7,5,2,2,4,4,5]);
 
-  print('Minimum Subarray: ${targetAndArray.getMinimumSubarray()}');
+  print('Minimum Subarray Length: ${targetAndArray.getMinimumSubarray()}');
 }
